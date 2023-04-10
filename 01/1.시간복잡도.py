eea53b2908a7e959e6e3b96bd4bad44c32623281
SIHYEON LEE <130329058+sleeee-dev@users.noreply.github.com>
2023-04-10 14:14:22 +0000

    Initial commit

--- 01/1.시간복잡도.py
+++ 01/1.시간복잡도.py
@@ -43,3 +43,14 @@ print("time:", end_time - start_time)
 # # 프로그램 소스코드
 # end_time = time.time() # 측정 종료
 # print("time:", end_time - start_time) # 수행 시간 출력
+
+
+# <Tip> 입출력 방식이 느릴때 고민해 볼 점
+# Python을 사용하고 있다면, input 대신 sys.stdin.readline을 사용할 수 있다.
+# 단, 이때는 맨 끝의 개행문자까지 같이 입력받기 때문에
+# 문자열을 저장하고 싶을 경우 .rstrip()을 추가로 해 주는 것이 좋다.
+# 그런데 rstrip을 하라는 건 문자열 자체를 변수에 저장하고 싶을 때 얘기지, 
+# 개행문자가 맨 끝에 들어와도 int 변환이나 split()을 그대로 할 수 있다.
+# 즉 int(sys.stdin.readline()), sys.stdin.readline().split() 이렇게 해도 아무 문제 없다.
+# 참고로 이름이 꽤 길기 때문에 input = sys.stdin.readline을 맨 처음에 함으로써 쓰면 편하다.
+# import sys 도 해줘야함
